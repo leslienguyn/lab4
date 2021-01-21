18d5c74b0255ea327e7f857ad5cb789c6239427c
Leslie Nguyen <len007@ucsd.edu>
2021-01-20 17:56:52 -0800

    adding for help

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,61 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+    "projects": [ {
+      "name": "Waiting in line",
+      "image": "lorempixel.people.1.jpeg",
+      "id": "project1"
+    },
+
+    {
+      "name": "Needfinding",
+      "image": "lorempixel.city.1.jpeg",
+      "id": "project2"
+    },
+
+    {
+      "name": "ipad",
+      "image": "lorempixel.technics.1.jpeg",
+      "id": "project3"
+    },
+
+    {
+      "name": "wheel",
+      "image": "lorempixel.technics.2.jpeg",
+      "id": "project4"
+    },
+
+    {
+      "name": "wisps",
+      "image": "lorempixel.abstract.1.jpeg",
+      "id": "project5"
+    },
+
+    {
+      "name": "art",
+      "image": "lorempixel.abstract.2.jpeg",
+      "id": "project6"
+    },
+
+    {
+      "name": "city",
+      "image": "lorempixel.city.2.jpeg",
+      "id": "project7"
+    },
+
+    {
+      "name": "peeps",
+      "image": "lorempixel.people.2.jpeg",
+      "id": "project8"
+    },
+
+    {
+      "name": "jumbo dumbo",
+      "image": "lorempixel.jumbotron.jpeg",
+      "id": "project9"
+    }
+  ]
+    
+  });
 };
