f37ed672ab1dc7a29f44363e63812fb322725780
Leslie Nguyen <leslienguyen98@gmail.com>
2021-01-20 21:13:14 -0800

    finished and adding new changes to lab4

--- app.js
+++ app.js
@@ -21,19 +21,19 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
+/*app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('IxD secret key'));
 app.use(express.session());
-app.use(app.router);
+app.use(app.router); */
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  //app.use(express.errorHandler());
 }
 
 // Add routes here
@@ -42,6 +42,7 @@ app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
 app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
 
 
 http.createServer(app).listen(app.get('port'), function(){
